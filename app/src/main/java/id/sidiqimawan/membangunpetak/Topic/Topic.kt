3a459b59e99637f8103a99fd67ca59c2c3f29b8a
AhmadSidiq157 <ahmad.sidiq@students.utdi.ac.id>
2024-10-21 11:40:37 +0700

    Initial commit

---
+++ app/src/main/java/id/sidiqimawan/membangunpetak/Topic/Topic.kt
@@ -0,0 +1,10 @@
+package id.sidiqimawan.membangunpetak.Topic
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
